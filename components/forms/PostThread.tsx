42820eeb98dcba43c8a639a3ae3de5838fb09a27
sap <sabark@eluocnc.com>
2024-07-12 19:20:54 +0900

    add: create thread

--- components/forms/PostThread.tsx
+++ components/forms/PostThread.tsx
@@ -19,6 +19,7 @@ import { usePathname, useRouter } from "next/navigation";
 
 // import { updateUser } from "@/lib/actions/user.actions";
 import { ThreadValidation } from "@/lib/validations/thread";
+import { createThread } from "@/lib/actions/thread.actions";
 
 interface Props {
   user: {
@@ -44,7 +45,16 @@ function PostThread({ userId }: { userId: string }) {
     },
   });
 
-  const onSubmit = () => {};
+  const onSubmit = async (values: z.infer<typeof ThreadValidation>) => {
+    await createThread({
+      text: values.thread,
+      author: userId,
+      communityId: null,
+      path: pathname,
+    });
+
+    router.push("/");
+  };
 
   return (
     <Form {...form}>
---
+++ lib/models/thread.model.ts
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+const threadSchema = new mongoose.Schema({
+  text: { type: String, required: true },
+  author: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  community: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Community",
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  parentId: {
+    type: String,
+  },
+  children: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Thread", // 한 Thread는 여러개의 threads를 chidlren으로 가질 수 있다
+    },
+  ],
+});
+
+const Thread = mongoose.models.Thread || mongoose.model("Thread", threadSchema);
+
+export default Thread;
